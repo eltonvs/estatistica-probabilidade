cf8ec65d1e7348db5baf00bc5560b5ba1e6775ab
Elton Viana <eltonvs@outlook.com>
2015-06-18 19:12:05 -0300

    Melhoras na separação das funções

--- funcoes.py
+++ funcoes.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-def fatorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * fatorial(n-1)
-
+#Medidas de Tendência Central
 def media(v):
-    return sum(v)/len(v)
+    return sum(v) / len(v)
 
 def moda(v):
     b, modas = 2, []
@@ -25,9 +20,26 @@ def moda(v):
 def mediana(v):
     v.sort()
     if len(v) % 2 == 0:
-        return media([v[int(len(v)/2) - 1], v[int(len(v)/2)]])
+        return media([v[int(len(v) / 2) - 1], v[int(len(v) / 2)]])
     else:
-        return v[int(len(v)/2)]
+        return v[int(len(v) / 2)]
+
+#Medidas de Dispersão
+def desvio_medio(v):
+    return media([abs(i - media(v)) for i in v])
+
+def variancia(v):
+    return media([pow(i - media(v), 2) for i in v])
+
+def desvio_padrao(v):
+    return pow(variancia(v), .5)
+
+#Análise Combinatória
+def fatorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fatorial(n - 1)
 
 def arranjo(n, p):
     return fatorial(n) / fatorial(n-p)
@@ -39,7 +51,7 @@ def combinacao(n, p):
     return arranjo(n, p) / fatorial(p)
 
 def combinacao_rep(n, p):
-    return combinacao(n+p-1,p)
+    return combinacao(n + p - 1, p)
 
 def permutacao(n):
     return fatorial(n)
@@ -51,13 +63,4 @@ def permutacao_rep(n, r):
     return n
 
 def permutacao_circular(n):
-    return fatorial(n-1)
-
-def desvio_medio(v):
-    return media([abs(i - media(v)) for i in v])
-
-def variancia(v):
-    return media([pow(i - media(v), 2) for i in v])
-
-def desvio_padrao(v):
-    return pow(variancia(v), .5)
+    return fatorial(n - 1)
